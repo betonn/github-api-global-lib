04c1008dd193900746c0dd57d3cd5b425a67f92b
beton <71146531+betonn@users.noreply.github.com>
2024-08-14 13:24:34 +0300

    Update betonReactTest.groovy

--- vars/betonReactTest.groovy
+++ vars/betonReactTest.groovy
@@ -3,6 +3,12 @@ def call(Map params) {
         agent('any')
         stages{
             stage('Example') {
+                 agent {
+                docker {
+                    image 'docker:dind'
+                    reuseNode true
+                }
+            }
                 steps {
                 sh "echo Hello world"
                 docker.build("test")
