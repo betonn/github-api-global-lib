eaccc9f86ed18f65b6f2b8a9bb5c7cab603f8663
beton <71146531+betonn@users.noreply.github.com>
2024-08-14 16:13:59 +0300

    Update betonReactTest.groovy

--- vars/betonReactTest.groovy
+++ vars/betonReactTest.groovy
@@ -2,15 +2,21 @@ def call(Map params) {
     pipeline{
         agent{ label 'test1node'}
         stages{
-            stage('Example') {
-                steps {
-                sh "echo Hello world"
-                docker.build("test")
-                }
-            }
+      // stage('Example') {
+      //     steps {
+      //     sh "echo Hello world"
+      //     docker.build("test")
+      //     }
+      // }
+      stage('Build') {
+        steps {
+          script {
+                dockerImage = docker.build("reacttestapp"),
+
+          }
+        }
+      }
+
         }
     }
-}
-
-
 
