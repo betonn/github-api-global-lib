68f8074a168f86520c0e3ad5bf1d45d79df33e33
beton <71146531+betonn@users.noreply.github.com>
2024-08-14 16:06:53 +0300

    Update betonReactTest.groovy

--- vars/betonReactTest.groovy
+++ vars/betonReactTest.groovy
@@ -1,43 +1,16 @@
-// def call(Map params) {
-//     pipeline{
-//         agent('any')
-//         stages{
-//             stage('Example') {
-//                  agent {
-//                 docker {
-//                     image 'docker:dind'
-//                     reuseNode true
-//                 }
-//             }
-//                 steps {
-//                 sh "echo Hello world"
-//                 docker.build("test")
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
-
 def call(Map params) {
-pipeline {
-    agent {
-                docker {
-                    image 'dind:stable'
-                    // Run the container on the node specified at the
-                    // top-level of the Pipeline, in the same workspace,
-                    // rather than on a new node entirely:
-                    reuseNode true
+    pipeline{
+        agent('	test1node')
+        stages{
+            stage('Example') {
+                steps {
+                sh "echo Hello world"
+                docker.build("test")
                 }
-            }
-    stages {
-        stage('Build') {
-            
-            steps {
-                sh 'docker --version'
             }
         }
     }
 }
-}
+
+
+
