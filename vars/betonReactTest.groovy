01d648903f736e3553ed816bdf5227c9b083c4ff
beton <71146531+betonn@users.noreply.github.com>
2024-08-14 13:18:10 +0300

    Update betonReactTest.groovy

--- vars/betonReactTest.groovy
+++ vars/betonReactTest.groovy
@@ -5,7 +5,18 @@ def call(Map params) {
             stage('Example') {
                 steps {
                 sh "echo Hello world"
-                sh "docker ps "
+                    // install docke r
+                    sh "sudo apt-get update"
+                    sh "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
+                    sh "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+                    sh "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'"
+                    sh "sudo apt-get update"
+                    sh "sudo apt-get install docker-ce"
+                    sh "sudo systemctl status docker"
+                    sh "sudo usermod -aG docker ${USER}"
+                    sh "docker --version"
+                    // build docker image
+                    sh "docker build -t myapp ."
                 }
             }
         }
